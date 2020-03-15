e8c75908c3d9937cdabc4d3d80250c0176e02386
fabi5567 <einkaufen34@icloud.com>
2020-03-15 11:44:33 +0100

    Karte mit OSM-Layer durch new LMap und new BaseLayer

--- src/main/java/com/vaadin/MyUI.java
+++ src/main/java/com/vaadin/MyUI.java
@@ -11,6 +11,11 @@ import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import org.vaadin.addon.leaflet.LMap;
+import org.vaadin.addon.leaflet.LMarker;
+import org.vaadin.addon.leaflet.LOpenStreetMapLayer;
+import org.vaadin.addon.leaflet.LTileLayer;
+import com.vaadin.ui.AbstractComponent;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -22,22 +27,47 @@ import com.vaadin.ui.VerticalLayout;
 @Theme("mytheme")
 public class MyUI extends UI {
 
+    // LMap Objekt nicht direkt in init, um es später leichter referenzieren zu können
+    // zeigt nur Kartenkomponente ohne Hintergrund an
+    private LMap map = new LMap();
+    // Blendet OSM-Karte mit ein
+    private LTileLayer osmTileLayer = new LOpenStreetMapLayer();
+
     @Override
     protected void init(VaadinRequest vaadinRequest) {
+        // triggered when a user enters your web application
         final VerticalLayout layout = new VerticalLayout();
         
         final TextField name = new TextField();
         name.setCaption("Type your name here:");
 
         Button button = new Button("Click Me");
+        // Button whose click listener dynamically adds a new Label component to the main layout
         button.addClickListener(e -> {
             layout.addComponent(new Label("Thanks " + name.getValue() 
                     + ", it works!"));
         });
 
-        layout.addComponents(name, button);
-        
-        setContent(layout);
+        /*
+        leafletMap.setImmediate(true);
+        final LTileLayer baselayer = new LTileLayer();
+        baselayer.setAttributionString( "OpenStreetMap" );
+        baselayer.setUrl( "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" );
+        leafletMap.addLayer( baselayer );
+        leafletMap.setWidth("200px");
+        leafletMap.setHeight("100px");
+        leafletMap.setZoomLevel(3);
+        LMarker leafletMarker = new LMarker(-21.54, 30.76);
+        leafletMap.addComponent(leafletMarker);
+        leafletMap.setCenter(-21.54, 30.76);
+        */
+
+        // Geht nicht, da map nicht vom Typ component
+        //layout.addComponents(name, button, map);
+
+        map.addBaseLayer(osmTileLayer, "OSM");
+
+        setContent(map);
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
